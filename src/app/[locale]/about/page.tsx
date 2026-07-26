94abf97bf7d724b90553654878d95a7448d9c6b7
Mustafa Yüceer <mustafayuceer@gmail.com>
2026-07-26 23:59:12 +0300

    Hakkında sayfasının alt görselini deprem bölgesi fotoğrafıyla değiştir

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -77,11 +77,11 @@ function About() {
       </ul>
 
       <Image
-        src="/images/mustafa-yuceer-portrait.png"
+        src="/images/deprem-bolgesi-cuma-namazi.jpeg"
         alt={t("heading")}
-        width={1112}
-        height={1186}
-        className="mt-16 h-64 w-full rounded-2xl object-cover object-top shadow-md ring-1 ring-black/5 sm:h-96 dark:ring-white/10"
+        width={750}
+        height={416}
+        className="mt-16 h-64 w-full rounded-2xl object-cover shadow-md ring-1 ring-black/5 sm:h-96 dark:ring-white/10"
       />
 
       <Link
